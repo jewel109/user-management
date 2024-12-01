3fff30bb39b4e68c4d7ec93433e2e17623c7da92
jewel109 <raihanurrahmanjewel@gmail.com>
2024-12-01 18:06:44 +0600

    catch everything filter added

---
+++ src/error.interceptor.ts
@@ -0,0 +1,57 @@
+
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { EntityNotFoundError, QueryFailedError, TypeORMError } from 'typeorm';
+
+@Catch()
+export class CatchEverythingFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) { }
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    // In certain situations `httpAdapter` might not be available in the
+    // constructor method, thus we should resolve it here.
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+
+    let httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
+    let responseBody = {
+      message: "unhandled error occured",
+      timestamp: new Date().toISOString(),
+    };
+
+    if (exception instanceof HttpException) {
+      httpStatus = exception.getStatus();
+      const response = exception.getResponse();
+      const message = typeof response === 'object' && response !== null
+        ? (response as { message: string }).message
+        : 'Unknown error found';
+      responseBody.message = message;
+    } else if (exception instanceof QueryFailedError) {
+      httpStatus = HttpStatus.BAD_REQUEST; // or another appropriate status
+      responseBody.message = `Database query failed: ${exception.message}`;
+    } else if (exception instanceof EntityNotFoundError) {
+      httpStatus = HttpStatus.NOT_FOUND;
+      responseBody.message = `Entity not found: ${exception.message}`;
+    } else if (exception instanceof Error) {
+      httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
+      responseBody.message = `just an Error  ${exception.message}`
+    } else if (exception instanceof TypeORMError) {
+      httpStatus = HttpStatus.BAD_REQUEST
+      responseBody.message = `error in db : ${exception.message}`
+    } else {
+      console.error(exception)
+    }
+
+
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { CatchEverythingFilter } from './error.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService)
   const port = configService.get('PORT')
   // console.log(port)
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CatchEverythingFilter(httpAdapter))
   await app.listen(port);
 }
 bootstrap();
