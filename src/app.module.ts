931062d8846bf812ec2f2c0645d758a8d4d50fff
jewel109 <raihanurrahmanjewel@gmail.com>
2024-12-01 18:05:42 +0600

    typeorm module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { SuccessInterceptor } from './success.interceptor';
 
 @Module({
-  imports: [ConfigModule.forRoot({
+  imports: [UserModule, ConfigModule.forRoot({
     isGlobal: true
-  })],
+  }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'jewel',
+      password: 'jewel',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
