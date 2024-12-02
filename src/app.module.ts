0459b50424fde028f033489a3c203d8cf8e4bdf4
jewel109 <raihanurrahmanjewel@gmail.com>
2024-12-02 10:46:13 +0600

    typeorm config with mysql db using data from .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { SuccessInterceptor } from './success.interceptor';
+import { type } from 'os';
+import { async } from 'rxjs';
 
 @Module({
   imports: [UserModule, ConfigModule.forRoot({
     isGlobal: true
   }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'jewel',
-      password: 'jewel',
-      database: 'test',
-      entities: [User],
-      synchronize: true,
-
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: configService.get('DB_PORT'),
+        username: configService.get('MYSQL_USER'),
+        password: configService.get('MYSQL_PASSWORD'),
+        database: configService.get('MYSQL_DATABASE'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService]
     }),
   ],
   controllers: [AppController],
