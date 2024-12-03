007eb343d58ad3f770e473a988ccfa03a4f653c3
jewel109 <raihanurrahmanjewel@gmail.com>
2024-12-03 10:20:12 +0600

    unit test for controller and service

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserService', () => {
   let service: UserService;
-
+  const mockUserRepository = {
+    find: jest.fn().mockResolvedValue([{ id: 1, name: 'John', email: 'john@example.com' }]),  // Mock response for find
+    save: jest.fn().mockResolvedValue({ id: 1, name: 'John', email: 'john@example.com' }),   // Mock response for save
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, {
+        provide: getRepositoryToken(User),
+        useValue: mockUserRepository
+      }],
     }).compile();
 
     service = module.get<UserService>(UserService);
